ece413848a81340f34db6a4abed2bbdfe9adc007
Tomomichi <tomomichi.onishi@gmail.com>
2013-01-11 20:53:07 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyaku1::Application.config.secret_token = 'c417e121968e6989d92710d916571a7c0a39ff8d7e56ccff34fc33782dad382eb8b6e81c83176eb2aa3244eb48455f6b834355bbaf6411eaace601cc0429e6f4'
