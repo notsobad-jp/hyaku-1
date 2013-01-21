2607de091399a080b87108f09cdc32f7ba17a676
Tomomichi <tomomichi.onishi@gmail.com>
2013-01-22 02:51:11 +0900

    refactored review model, using scope etc

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,7 +1,6 @@
 class ReviewController < ApplicationController
   def index
-    #Review:1day
-    @review_targets_1 = History.where(:created_at => Date.yesterday.beginning_of_day..Date.yesterday.end_of_day).includes(:song)
+    @review_targets = History.which_day(1.day.ago).includes(:song)
 
     respond_to do |format|
       format.html # show.html.erb
@@ -12,13 +11,14 @@ class ReviewController < ApplicationController
   def new
     @history = History.new
 
-    #Review:1day
-    @review_targets_1 = History.where(:created_at => Date.yesterday.beginning_of_day..Date.yesterday.end_of_day).includes(:song)
-    @dummy_songs = Song.find(:all, :order => 'RAND()', :limit => 3)
+    # 復習対象の問題を取得（1,3,7日前）
+    [1,3,7].each do |d|
+      instance_variable_set("@review_targets_#{d}", History.original.which_day(d.day.ago).who(current_user).includes(:song) )
+    end
+    @review_targets = @review_targets_1 + @review_targets_3 + @review_targets_7
 
     respond_to do |format|
       format.html # try.html.erb
-      format.js
       format.json { render json: @history }
     end
   end
