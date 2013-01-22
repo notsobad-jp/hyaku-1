4d008c8f4dd48ba73fc54d67dd14a84bf7f53d25
Tomomichi <tomomichi.onishi@gmail.com>
2013-01-23 03:00:32 +0900

    changed home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,25 @@ class HomeController < ApplicationController
   skip_before_filter :require_login
 
   def index
-    #Learn: 今日の歌を取得
-    last_song = History.who(current_user).where("created_at < ?", Date.today).original.order("created_at DESC").first
-    last_song_id = (last_song) ? last_song.song_id : 0
-    @songs = Song.order("id ASC").offset(last_song_id).limit(SONGS_PER_DAY).includes(:histories)
+    if current_user
+      #Learn: 今日の歌を取得
+      last_song = History.who(current_user).where("created_at < ?", Date.today).original.order("created_at DESC").first
+      last_song_id = (last_song) ? last_song.song_id : 0
+      @songs = Song.order("id ASC").offset(last_song_id).limit(SONGS_PER_DAY).includes(:histories)
 
-    #Review: 今日の復習対象を取得
-    [1,3,7].each do |d|
-      instance_variable_set("@review_targets_#{d}", History.original.which_day(d.day.ago).who(current_user).includes(:song) )
+      #Review: 今日の復習対象を取得
+      [1,3,7].each do |d|
+        instance_variable_set("@review_targets_#{d}", History.original.which_day(d.day.ago).who(current_user).includes(:song) )
+      end
+
+      #Exam: 前回のスコア取得
+      @score = History.who(current_user).where(:exam_flg => 1).where(:result => 1).count
+    else
+      @songs = Array.new
+      @review_targets_1 = Array.new
+      @review_targets_2 = Array.new
+      @review_targets_3 = Array.new
+      @score = 0
     end
-
-    #Exam: 前回のスコア取得
-    @score = History.who(current_user).where(:exam_flg => 1).where(:result => 1).count
-
   end
 end
