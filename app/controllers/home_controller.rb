ba6fde6efca00b41d9d49d793f0bca5444b03552
Tomomichi <tomomichi.onishi@gmail.com>
2013-01-23 02:13:06 +0900

    added score and review targets in home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 class HomeController < ApplicationController
 
   def index
-    #今日の歌を取得
+    #Learn: 今日の歌を取得
     last_song = History.who(current_user).where("created_at < ?", Date.today).original.order("created_at DESC").first
     last_song_id = (last_song) ? last_song.song_id : 0
     @songs = Song.order("id ASC").offset(last_song_id).limit(SONGS_PER_DAY).includes(:histories)
+
+    #Review: 今日の復習対象を取得
+    [1,3,7].each do |d|
+      instance_variable_set("@review_targets_#{d}", History.original.which_day(d.day.ago).who(current_user).includes(:song) )
+    end
+
+    #Exam: 前回のスコア取得
+    @score = History.who(current_user).where(:exam_flg => 1).where(:result => 1).count
+
   end
 end
