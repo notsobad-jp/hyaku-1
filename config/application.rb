560dc15332afe00338b80b1245cf7166ca220c5e
Tomomichi <tomomichi.onishi@gmail.com>
2013-02-08 01:48:40 +0900

    set precompile false and modified errors in css

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,8 @@ module Hyaku1
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
